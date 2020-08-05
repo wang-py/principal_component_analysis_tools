55fcf1ab3c712efdab4279d20a07d5e8b03109c2
MillerMC96 <pywang@ucdavis.edu>
2020-08-05 13:39:35 -0700

    ver 1.0

--- movie_maker.sh
+++ movie_maker.sh
@@ -3,16 +3,22 @@
 # this script cat all pdbs into a pymol-readable movie pdb
 # Author: Panyue Wang, Email: pywang@ucdavis.edu
 
+# working directory
+
+DIR=$1
+
 # output name
-output="movie.pdb"
+output=$DIR/"movie.pdb"
 # frame number
 frame=1
 
-for pdb in `ls | sort -V`; do
+input=`ls $DIR | sort -V`
+
+for pdb in $input; do
     echo "TITLE     frame t= $frame" >> $output
     echo "MODEL         1" >> $output
     cat $pdb >> $output
     echo "TER" >> $output
     echo "ENDMDL" >> $output
-    let $frame=$frame+1
+    let frame=frame+1
 done
