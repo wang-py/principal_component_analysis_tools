44bb7b6ea0e733d552dcb520fedee33d3dd47a02
MillerMC96 <pywang@ucdavis.edu>
2020-08-09 23:25:32 -0700

    define input and output

--- movie_pdb_primer.py
+++ movie_pdb_primer.py
@@ -3,3 +3,11 @@ import sys
 from pymol import cmd
 from modes import get_xvg_stats
 from generate_probability import generate_color_profile
+
+if __name__ == "__main__":
+    # xvg file that has the trajectory information
+    xvgfile = sys.argv[1]
+    # probability mode
+    mode = int(sys.argv[2])
+    # output pdb name
+    movie_primer_pdb = sys.argv[3]
