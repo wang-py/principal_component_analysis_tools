711820b14d07a2dd2c52ec5976feebd0143a3e74
MillerMC96 <pywang@ucdavis.edu>
2020-08-09 23:38:56 -0700

    read color from the calculation

--- movie_pdb_primer.py
+++ movie_pdb_primer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 from pymol import cmd
+from pymol import stored
 from modes import get_xvg_stats
 from generate_probability import generate_color_profile
 
@@ -11,3 +12,13 @@ if __name__ == "__main__":
     mode = int(sys.argv[2])
     # output pdb name
     movie_primer_pdb = sys.argv[3]
+
+    color_profile = generate_color_profile(xvgfile, mode)
+    stored.new_b_factor = []
+    for color in color_profile:
+        stored.new_b_factor.append(color)
+
+    #TODO: change the b factors of input pdb here
+    #cmd.alter()
+    #TODO: save the primer pdb here
+    #cmd.save()
