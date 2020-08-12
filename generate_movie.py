d35aca044a10b1cf1f6bff60ba3581afb1c64a0d
MillerMC96 <pywang@ucdavis.edu>
2020-08-11 17:10:38 -0700

    basic structure

--- generate_movie.py
+++ generate_movie.py
@@ -3,3 +3,22 @@
 
 from biopandas.pdb import PandasPdb
 from generate_probability import generate_color_profile
+import sys
+
+if __name__ == "__main__":
+    # starting structure
+    input_pdb = sys.argv[1]
+    # eigenvectors (modes)
+    input_ev = sys.argv[2]
+    # PCA mode
+    mode = int(sys.argv[3])
+    # biopandas dataframe
+    ppdb = PandasPdb()
+    ppdb.read_pdb(input_pdb)
+    # generate color profile based on chosen mode
+    color_profile = generate_color_profile(input_ev, mode)
+    
+    for i in range(len(color_profile)):
+        color = color_profile[i]
+        ppdb.df['bfactor'][i] = color
+    
