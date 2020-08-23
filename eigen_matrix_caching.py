ed9f79eee30deb5c742fbc613e3b12d75cc1e4b4
MillerMC96 <pywang@ucdavis.edu>
2020-08-23 15:26:25 -0700

    clean up

--- eigen_matrix_caching.py
+++ eigen_matrix_caching.py
@@ -7,15 +7,6 @@ if __name__ == '__main__':
     pdbToAlign = sys.argv[2] if len(sys.argv)>2 else None
     mean1, mean2, cov, s, u, v = \
         get_xvg_stats(sys.argv[1],fitfile=pdbToAlign,unbias=unbias)
-    np.set_printoptions(threshold=sys.maxsize)
-    np.core.arrayprint._line_width = 1800
-    #print("mean ",mean1)
-    #print("cov ", cov.shape)
-    #print("D ", s.shape)
-    #print("S ", u.shape)
-    #print("S2 ",v.shape)
-    #print("sum(S-S2)=",np.sum(u-v))
-    #print("D = ", s)
     with open('covariance.npy','wb') as f:
         np.save(f,cov)
     with open('eigenvectors.npy', 'wb') as f:
